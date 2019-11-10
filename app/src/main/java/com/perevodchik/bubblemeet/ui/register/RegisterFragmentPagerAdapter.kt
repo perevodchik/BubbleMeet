088d459d2f0df9ab38aaa3cbf8b2722047b89c8a
perevodchik <mastres24@gmail.comk>
2019-11-10 21:44:35 +0200

    Fix register fragment view

--- app/src/main/java/com/perevodchik/bubblemeet/ui/register/RegisterFragmentPagerAdapter.kt
+++ app/src/main/java/com/perevodchik/bubblemeet/ui/register/RegisterFragmentPagerAdapter.kt
@@ -1,22 +1,16 @@
 package com.perevodchik.bubblemeet.ui.register
 
-import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.perevodchik.bubblemeet.ui.register.fragments.*
-import java.util.ArrayList
 
-class RegisterFragmentPagerAdapter(_fm: FragmentManager) : FragmentPagerAdapter(_fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
-    private val fragments: ArrayList<Fragment> = arrayListOf(
+class RegisterFragmentPagerAdapter(_fm: FragmentManager) :
+    FragmentPagerAdapter(_fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    private val fragments: List<Fragment> = listOf(
         Fragment0(), Fragment1(), Fragment2(), Fragment3(), Fragment4(),
         Fragment5(), Fragment6(), Fragment7(), Fragment8(), Fragment9()
     )
-
-    override fun isViewFromObject(view: View, `object`: Any): Boolean {
-        //return view === `object` as Fragment
-        return true
-    }
 
     override fun getItem(position: Int): Fragment {
         return fragments[position]
@@ -25,7 +19,6 @@ class RegisterFragmentPagerAdapter(_fm: FragmentManager) : FragmentPagerAdapter(
     override fun getCount(): Int {
         return fragments.size
     }
-
 
 
 }
